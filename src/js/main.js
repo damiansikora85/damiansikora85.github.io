9d0d82c23e7737c9f4abdd425c15f915e8e54fb5
Damian Sikora <dsikora85@gmail.com>
2019-09-22 12:56:05 +0200

    Getting projects from GH

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,15 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
 
 // place your code below
@@ -19,4 +19,35 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+const projectsList = document.querySelector('.projects__list-js');
+console.log(projectsList);
 
+if (projectsList) {
+    fetch('https://api.github.com/users/damiansikora85/repos')
+        .then(response => response.json())
+        .then(response => {
+            for (const repo of response) {
+                const { name, html_url, description } = repo;
+                projectsList.innerHTML +=
+                    `<li class="project">
+                <img src="../assets/img/githubIcon.svg" alt="github" class="project__icon " />
+                        <h2 class="project__title ">${name}</h2>
+                        <h3 class="project__desc ">${description}</h3>
+                        <section class="project__links">
+                            <a href=" " class="project__link">
+                                <img src=" " alt=" " />
+                                <h3>Demo</h3>
+                            </a>
+
+                            <a href=" " class="project__link">
+                                <img src=" " alt=" " />
+                                <h3>Code</h3>
+                            </a>
+                        </section>
+                </li>`
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
