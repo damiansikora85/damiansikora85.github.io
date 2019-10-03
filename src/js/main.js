be15afe1b73c3853b16bdf3176f23d18e53d0995
Damian Sikora <dsikora85@gmail.com>
2019-10-03 20:46:09 +0200

    Changed projects to bitbucket

--- src/js/main.js
+++ src/js/main.js
@@ -8,26 +8,26 @@ console.log(`Harry. I'm gonna let you in on a little secret. Every day, once a d
 const projectsList = document.querySelector('.projects__list-js');
 
 if (projectsList) {
-    fetch('https://api.github.com/users/damiansikora85/repos')
+    fetch('https://api.bitbucket.org/2.0/repositories/dsikora85')
         .then(response => response.json())
         .then(response => {
-                for (const repo of response) {
-                    const { name, homepage, html_url, description } = repo;
+                let values = response.values;
+                for (const repo of values) {
+                    const { name, website, links, description } = repo;
                     projectsList.innerHTML +=
                         `<li class="project">
                         <div class="project__container">
-                            <img src="../assets/img/githubIcon.svg" alt="github" class="project__icon " />
+                            <img src="../assets/img/bitbucket-brands.svg" alt="bitbucket" class="project__icon " />
                             <h2 class="project__title ">${name}</h2>
                             ${
                                 description ? `<h3 class="project__desc ">${description}</h3>` : ''
                               }    
                         </div>
                         <section class="project__links">
-                        ${
-                            homepage ? `<a href="${homepage}" class="project__link project__link--demo" target="_blank" rel="nofollow noreferrer">Demo</a>` : ''
-                        }
-                            
-                            <a href="${html_url}" class="project__link" target="_blank" rel="nofollow noreferrer">Code</a>
+   ${
+website ? `<a href="${website}" class="project__link project__link--demo" target="_blank" rel="nofollow noreferrer">Demo</a>` : ''
+   }
+                            <a href="${links.html.href}" class="project__link" target="_blank" rel="nofollow noreferrer">Code</a>
                         </section>
                 </li>`
             }
