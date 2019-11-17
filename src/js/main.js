a421227daf6e44de3e3b6956986e42f7a7c5ae17
Damian Sikora <dsikora85@gmail.com>
2019-11-17 14:00:22 +0100

    changed bitbucket repos to github

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ console.log(`Harry. I'm gonna let you in on a little secret. Every day, once a d
 const projectsList = document.querySelector('.projects__list-js');
 
 if (projectsList) {
-    fetch('https://api.bitbucket.org/2.0/repositories/dsikora85')
+    /*fetch('https://api.bitbucket.org/2.0/repositories/dsikora85')
         .then(response => response.json())
         .then(response => {
                 let values = response.values;
@@ -30,6 +30,35 @@ website ? `<a href="${website}" class="project__link project__link--demo" target
                             <a href="${links.html.href}" class="project__link" target="_blank" rel="nofollow noreferrer">Code</a>
                         </section>
                 </li>`
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });*/
+
+    fetch('https://api.github.com/users/damiansikora85/repos')
+        .then(response => response.json())
+        .then(response => {
+                for (const repo of response) {
+                    if (repo.fork == false) {
+                        const { name, description, html_url, homepage } = repo;
+                        projectsList.innerHTML +=
+                            `<li class="project">
+                            <div class="project__container">
+                                <img src="../assets/img/githubIcon.svg" alt="github" class="project__icon " />
+                                <h2 class="project__title ">${name}</h2>
+                                ${
+                                    description ? `<h3 class="project__desc ">${description}</h3>` : ''
+                                }    
+                            </div>
+                            <section class="project__links">
+                            ${
+                                homepage ? `<a href="${homepage}" class="project__link project__link--demo" target="_blank" rel="nofollow noreferrer">Demo</a>` : ''
+                            }
+                                <a href="${html_url}" class="project__link" target="_blank" rel="nofollow noreferrer">Code</a>
+                            </section>
+                    </li>`
+                }
             }
         })
         .catch(error => {
